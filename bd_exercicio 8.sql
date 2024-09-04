4100fc471ebc26e6dac22a6d554e7503da68357a
Leandro Junio <155511363+Junimdarua18@users.noreply.github.com>
2024-09-04 14:14:40 -0300

    Delete bd_exercicio 8.sql

--- bd_exercicio 8.sql
+++
@@ -1,4 +0,0 @@
-select func_nome, depto_nome, proj_numero
-from bd_exercicio.tb_funcionario
-inner join bd_exercicio.tb_departamento on depto_codigo = trab_depto_cod
-inner join bd_exercicio.tb_projeto on proj_codigo = proj_cod;
